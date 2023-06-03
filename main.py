09313d76f55b3e42ab53fb7ae1807d60cde17b54
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-06-03 23:17:14 +0200

    feat: make bot commands available. Delete the annoucement.py (old file command)

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ from discord.ext import commands
 logger = settings.logging.getLogger("bot")
 
 def run():
-    intents = discord.Intents.default()
     
-    bot = commands.Bot(command_prefix=")", intents=intents)
+    intents = discord.Intents.default()
+    intents.messages = True
+
+    bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
     
     @bot.event
     async def on_ready():
         logger.info(f"Le bot {bot.user} (ID: {bot.user.id}) est en ligne.")
         
-    bot.run(settings.TOKEN, root_logger=True)
     
+    @bot.command()
+    async def ping(ctx):
+        await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
+    
+    
+    bot.run(settings.TOKEN, root_logger=True)
     
 if __name__ == "__main__":
     run()
